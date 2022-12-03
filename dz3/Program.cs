3e988aa6afedd0c58fb2d9e530f55f8ab32156c5
UrsAlex <ursalex@mail.ru>
2022-12-03 17:26:26 +0300

    решение третьего задания

---
+++ dz3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдает, является ли число четным (делится ли оно на два без остатка)
+
+Console.WriteLine("Inter Number ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0) 
+{
+    Console.WriteLine($"even number {number}");
+}
+else
+{
+    Console.WriteLine($"odd number {number}");
+}
