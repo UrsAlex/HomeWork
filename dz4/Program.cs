98b65aae0022f45f2e5081c72e473c688874096e
UrsAlex <ursalex@mail.ru>
2022-12-03 22:05:50 +0300

    решение четвертого задания

---
+++ dz4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N), а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int numberA = 2;
+while (numberA < number)
+   {
+    Console.WriteLine($"{numberA}");
+    numberA += 2;
+   } 
